54d740486d38cdc8cae620ea9279d84ac084df89
Hades <83053023+TuanTQ15@users.noreply.github.com>
2024-03-29 00:34:37 +0700

    Update snapshot.sh

--- snapshot.sh
+++ snapshot.sh
@@ -1,12 +1,14 @@
-sudo systemctl stop namadad
-cp .local/share/namada/shielded-expedition.88f17d1d14/cometbft/data/priv_validator_state.json .local/share/namada/priv_validator_state.json 
-sudo apt install lz4
-mkdir ~/namada-temp
-curl -L https://namada-se-rpc.citadel.one/snap/namada-se-90000.tar.lz4 | tar -Ilz4 -xf - -C ~/namada-temp
-rm -rf ~/.local/share/namada/shielded-expedition.88f17d1d14/db
-rm -rf ~/.local/share/namada/shielded-expedition.88f17d1d14/cometbft/data
-cp -a ~/namada-temp/db ~/.local/share/namada/shielded-expedition.88f17d1d14/
-cp -a ~/namada-temp/data ~/.local/share/namada/shielded-expedition.88f17d1d14/cometbft
-mv .local/share/namada/priv_validator_state.json .local/share/namada/shielded-expedition.88f17d1d14/cometbft/data/priv_validator_state.json 
-sudo systemctl restart namadad 
-sudo journalctl -u namadad -f -o cat
+sudo systemctl stop namadad.service
+cp $HOME/.local/share/namada/shielded-expedition.88f17d1d14/cometbft/data/priv_validator_state.json $HOME/.local/share/namada/shielded-expedition.88f17d1d14/priv_validator_state.json.backup
+rm -rf $HOME/.local/share/namada/shielded-expedition.88f17d1d14/cometbft/data $HOME/.local/share/namada/shielded-expedition.88f17d1d14/db
+
+curl -L https://snapshots.lavenderfive.com/testnet-extra-db/namada/latest_db.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.local/share/namada
+
+curl -L https://snapshots.lavenderfive.com/testnet-snapshots/namada/latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.local/share/namada
+
+mv "$HOME/.local/share/namada/data/" "$HOME/.local/share/namada/shielded-expedition.88f17d1d14/cometbft/"
+mv "$HOME/.local/share/namada/db/" "$HOME/.local/share/namada/shielded-expedition.88f17d1d14/"
+cp $HOME/.local/share/namada/shielded-expedition.88f17d1d14/priv_validator_state.json.backup $HOME/.local/share/namada/shielded-expedition.88f17d1d14/cometbft/data/priv_validator_state.json
+
+
+sudo systemctl restart namadad.service && sudo journalctl -u namadad.service -f --no-hostname -o cat
